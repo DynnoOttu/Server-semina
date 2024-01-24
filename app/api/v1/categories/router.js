0ac9b94cf565f1365b94dd00a50806f08e2d0a3e
dynnoinovamedika <dynnoottu.inovamedia@gmail.com>
2024-01-24 09:32:07 +0700

    Konfigurasi MongoDB Part 2

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express()
+const {create, index, find} = require('./controller')
 
-router.get('/categories', (req, res) => {
-    res.status(200).json({
-        message: 'halaman categories'
-    })
-})
+router.get('/categories', index)
+router.get('/categories:id', find)
+router.post('/categories', create)
 
 module.exports = router
